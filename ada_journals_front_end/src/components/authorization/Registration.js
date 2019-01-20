2ab18c3dd944285e430d666ae6ef3b6ff6e6b192
Dionisia Montanez <dmontanez15@gmail.com>
2019-01-20 15:13:12 -0800

    registration form working.

--- ada_journals_front_end/src/components/authorization/Registration.js
+++ ada_journals_front_end/src/components/authorization/Registration.js
@@ -1,10 +1,86 @@
 import React, { Component } from 'react';
 
 class Registration extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      errors: {}
+    }
+  }
+
+  onChange = (event) => {
+    console.log("Some stuff was typed in the form!");
+
+    const field = event.target.name;
+    const value = event.target.value;
+
+    const newState = {};
+    newState[field] = value;
+    this.setState(newState);
+    console.log(newState);
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    console.log(newUser);
+  }
+
   render() {
     return (
-      <div>
-        <h1>Sign up here!</h1>
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up </h1>
+              <p className="lead text-center">
+                Create your account here:
+              </p>
+              <form onSubmit={this.onFormSubmit}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Name"
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+                </form>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
