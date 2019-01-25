a7c244c056e2c0bd9f7089c16c69c93169978bf3
Dionisia Montanez <dmontanez15@gmail.com>
2019-01-25 10:52:11 -0800

    final main page layout done

--- ada_journals_front_end/src/components/layout/HomePage.js
+++ ada_journals_front_end/src/components/layout/HomePage.js
@@ -13,21 +13,22 @@ class HomePage extends Component {
   render() {
     return (
       <div className="homepage">
-        <div className="dark-overlay homepage-inner text-white">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 text-center text-muted">
-                <h1 className="display-3 mb-4"> Ada Journals </h1>
-                <h2 className="text-muted">Capture and share your notes/ideas with fellow Adies!</h2>
-                <hr />
-                <p className="lead">
+        <div className="container">
+          <div className="row">
+            <div className="card mt-5">
+              <div className="card card-header bg-secondary text-black text-center mb-3">
+                <h1 className="display-2 mt-4 mb-4"> Ada Journals </h1>
+                <h2 className="text-white mt-4 mb-5">Capture and share your notes/ideas with fellow Adies!</h2>
+              </div>
+              <div className="card-body text-center mt-4 mb-4">
+                <p className="lead mb-5 mr-3 ml-3">
                   A place to facilitate the organization of online learning by creating a more efficient process while promoting the ethical use of technology.
                 </p>
                 <hr />
-                <Link to="/registration" className="btn btn-lg btn-outline-secondary mr-2">
+                <Link to="/registration" className="btn btn-lg btn-dark mt-5 mr-3">
                   Sign Up
                 </Link>
-                <Link to="/login" className="btn btn-lg btn-warning">
+                <Link to="/login" className="btn btn-lg btn-warning mt-5">
                   Login
                 </Link>
               </div>
